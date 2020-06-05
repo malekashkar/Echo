4d5ed681c8d9c63f4f957c0e09d1ab5a6412e008
hgytduh <34247208+hgytduh@users.noreply.github.com>
2020-06-05 23:03:51 +0100

    owo

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
 const Discord = require('discord.js');
 const mysql = require('mysql');
-const fs = require('fs');
-const Enmap = require('enmap');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 require("dotenv").config();
 
 const con = mysql.createConnection({
     host: "188.40.194.152",
-    user: "pixelhaze",
+    user: "root",
     password: "Echo099",
-    database: "Echo"
-})
+    database: "echo"
+}) 
 
-con.connect(function(err, rows) {
+con.connect(function(err) {
     if(err) return console.log(err);
     console.log(`Database is up!`)
 })
-
-client.config = config;
-client.commands = new Enmap();
-
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-  });
-});
-
 client.login(process.env.token);
