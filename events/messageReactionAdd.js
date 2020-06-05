14331c44f8e7cd36bc60dfba55d929c5e4ae319c
hgytduh <34247208+hgytduh@users.noreply.github.com>
2020-06-05 13:20:34 +0100

    MessageReactionAdd event

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,7 +1,13 @@
-module.exports = async(client, reaction, user) => {
+module.exports = async(client, reaction, user, con) => {
     if(user.bot) return;
     if(reaction.message.partial) reaction.message.fetch();
     let message = reaction.message;
+    let guildID = message.guild.id;
+    con.query(`SELECT * FROM setup WHERE guildID = ?`, [guildID], function(err, rows) {
+        if(err) return message.channel.send(err);
+        if(!rows[0]) return;
+        if(message.id === rows[0].messageID){
 
-
+        }
+    })
 }
